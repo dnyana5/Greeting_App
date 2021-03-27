d21175d4ceb754f73e39c420640e6eda24714c3a
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-03-27 15:19:06 +0530

    Added:Greeting Controller return JSON for different HTTP methods

--- src/main/java/com/firstspringapp/controller/GreetingController.java
+++ src/main/java/com/firstspringapp/controller/GreetingController.java
@@ -4,7 +4,6 @@ package com.firstspringapp.controller;
 import com.firstspringapp.model.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
